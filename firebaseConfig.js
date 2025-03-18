3b3e1f6174da3c9350643e8825c496d3b85c4a1c
luxiang <luxiang@kth.se>
2025-03-18 12:45:35 +0100

    first two pages completed

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAMVfKaH4z76Hh_N9s6H52mTfFG9iTYq8E",
+    authDomain: "birthday-card-a8587.firebaseapp.com",
+    projectId: "birthday-card-a8587",
+    storageBucket: "birthday-card-a8587.firebasestorage.app",
+    messagingSenderId: "837043716564",
+    appId: "1:837043716564:web:d2025138bf2422d4bd5641",
+    measurementId: "G-P2JQQHBXBN"
+};
